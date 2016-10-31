289a5637280eb5ff89ec81ae49a87fadd6737115
BuildTools <unconfigured@null.spigotmc.org>
2016-10-31 18:50:51 +0100

    First commit

---
+++ check_plate.py
@@ -0,0 +1,18 @@
+from subprocess import check_output
+import glob
+
+def read_plate(filename):
+    plate_dict = check_output("alpr -c eu -p nl -j %s" %filename,shell=True).decode()
+    return eval(plate_dict)
+
+list = glob.glob('sample/*.jpg')
+count = 0
+for item in list:
+    try:
+        plate = read_plate(item)['results'][0]['plate']
+        count += 1
+        print(count)
+        print(item)
+        print(plate)
+    except:
+        continue
